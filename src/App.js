d26ce252bb20c33f05a8d2835b6d08cd1a4192eb
Temirlan <temohatemoha25@gmail.com>
2024-08-21 16:33:00 +0500

    mian

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       {/* <Detail /> */}
-      <Cart />
-      {/* <MainPage /> */}
+      {/* <Cart /> */}
+      <MainPage />
     </div>
   );
 }
