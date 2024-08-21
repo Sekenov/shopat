0f3e456b4f36fdc9a046d86df63c018dc8337bf9
Artem <ar.gubarev50@email.com>
2024-08-21 15:45:59 +0500

    dodelal

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import './MainPage.css';
+import frameImage from '../../img/cross/crosovki.png'; 
 
 function MainPage() {
   return (
@@ -32,6 +33,65 @@ function MainPage() {
           <div className="brand converse"></div>
         </div>
       </main>
+
+      <section className='Popular'>
+        <div className='PopularText'>
+          <h1>Popular Shoes</h1>
+          <p>See all</p>
+        </div>
+
+        <div className='wrapcart'>
+          <div className="Popular_card">
+            <div className="Popular_image">
+              <img src={frameImage} alt="Product" />
+            </div>
+            <div className="Popular_text">
+              <span>BEST SELLER</span>
+              <h2>Nike Jordan</h2>
+            </div>
+            <div className="buy">
+              <p>$493.00</p>
+              <button className="add-to-cart">+</button>
+            </div>
+          </div>
+
+
+
+          <div className="Popular_card">
+            <div className="Popular_image">
+              <img src={frameImage} alt="Product" />
+            </div>
+            <div className="Popular_text">
+              <span>BEST SELLER</span>
+              <h2>Nike Jordan</h2>
+            </div>
+            <div className="buy">
+              <p>$493.00</p>
+              <button className="add-to-cart">+</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='New'>
+        <div className='NewText'>
+          <h1>New Arrivals</h1>
+          <p>See all</p>
+        </div>
+
+        <div className="New_card">
+            <div className="Best_text">
+              <span>Best Choice</span>
+              <h2>Nike Air Jordan</h2>
+              <p>$849.69</p>
+            </div>
+
+            <div className="New_image">
+              <img src={frameImage} alt="Product" />
+            </div>
+        </div>
+
+      </section>
 
     </div>
   );
